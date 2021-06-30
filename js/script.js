57e3f85041ff7f86bf1849e1d1a9657148be9e16
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-06-30 12:36:17 +0200

    add: milestone 2

--- js/script.js
+++ js/script.js
@@ -7,51 +7,133 @@
 const app = new Vue({
     el: '#app',
     data: {
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+        contactActive: 0,
         contacts: [
             {
             name: 'Michele',
             avatar: '_1',
             visible: true,
             messages: [
-            {
-            date: '10/01/2020 15:30:55',
-            text: 'Hai portato a spasso il cane?',
-            status: 'sent'
-            },
-            {
-            date: '10/01/2020 15:50:00',
-            text: 'Ricordati di dargli da mangiare',
-            status: 'sent'
-            },
-            {
-            date: '10/01/2020 16:15:22',
-            text: 'Tutto fatto!',
-            status: 'received'
-            }
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+                }
             ],
             },
+
             {
             name: 'Fabio',
             avatar: '_2',
             visible: true,
             messages: [
-            {
-            date: '20/03/2020 16:30:00',
-            text: 'Ciao come stai?',
-            status: 'sent'
-            
-            },
-            {
-            date: '20/03/2020 16:30:55',
-            text: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
-            },
-            {
-            date: '20/03/2020 16:35:00',
-            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
-            }
+                {
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+                },
+                {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+                },
+                {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+                }
             ],
             },
-        ]}
+
+            {
+            name: 'Antonio',
+            avatar: '_3',
+            visible: true,
+            messages: [
+                {
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+                },
+                {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+                },
+                {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+                }
+            ],
+            },
+
+            {
+            name: 'Lorenzo',
+            avatar: '_4',
+            visible: true,
+            messages: [
+                {
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+                },
+                {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+                },
+                {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+                }
+            ],
+            },
+
+            {
+            name: 'Ciro',
+            avatar: '_5',
+            visible: true,
+            messages: [
+                {
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+                },
+                {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+                },
+                {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+                }
+            ],
+            },
+        ]
+    },
+
+    methods:{
+        setActive(indice){
+            this.contactActive = indice;
+            console.log(this.contactActive);
+        }
+    }
 });
